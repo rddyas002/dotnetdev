a1aec7c284d131e400a555fbb58c6ea71bc69118
Yashren Reddi <rddyas002@myuct.ac.za>
2017-12-14 12:18:53 +0200

    Update Program.cs

--- webdev/webdev/Program.cs
+++ webdev/webdev/Program.cs
@@ -14,13 +14,16 @@ namespace webdev
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-				.UseUrls("http://0.0.0.0:80")
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://0.0.0.0:80")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
+
+            host.Run();
+        }
     }
 }
