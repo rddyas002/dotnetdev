85a847fd83420469078b5c668ea8595850168ac2
Yashren Reddi (Laptop) <yreddi@spaceteq.co.za>
2017-12-18 14:21:38 +0200

    zulutek

--- webdev/webdev/Controllers/HomeController.cs
+++ webdev/webdev/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace webdev.Controllers
 
         public IActionResult Contact()
         {
-            //ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Me!";
 
             return View();
         }
