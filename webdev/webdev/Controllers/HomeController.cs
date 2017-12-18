286b20fe717cbb769bd67bdeeaaa6069db5ea82f
Yashren Reddi (Laptop) <yreddi@spaceteq.co.za>
2017-12-18 08:16:18 +0200

    test

--- webdev/webdev/Controllers/HomeController.cs
+++ webdev/webdev/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace webdev.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            //ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            //ViewData["Message"] = "Your contact page.";
 
             return View();
         }
